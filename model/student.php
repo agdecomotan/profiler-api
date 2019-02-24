20e08865014bfcd7921d6617ed59e8012f280e56
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2019-02-24 22:27:07 +0800

    Update student model

--- model/student.php
+++ model/student.php
@@ -1,13 +1,12 @@
 <?php
 namespace AGD\Profiler;
-
 class Student
 {
     public $id;
-    public $number;
-    public $firstname;
-    public $lastname;    
-    public $yearlevel; 
+    public $studentNumber;
+    public $firstName;
+    public $lastName;    
+    public $yearLevel; 
     public $program;
     public $email;
 
@@ -15,10 +14,10 @@ class Student
     {
         if ($data !== null) {
             $this->id = (int) $data['id'] ?? 0;
-            $this->number = $data['number'] ?? null;
-            $this->firstname = $data['firstname'] ?? null;
-            $this->lastname = $data['lastname'] ?? null;
-            $this->yearlevel = $data['yearlevel'] ?? null;
+            $this->studentNumber = $data['studentNumber'] ?? null;
+            $this->firstName = $data['firstName'] ?? null;
+            $this->lastName = $data['lastName'] ?? null;
+            $this->yearLevel = $data['yearLevel'] ?? null;
             $this->program = $data['program'] ?? null;
             $this->email = $data['email'] ?? null;
         }
