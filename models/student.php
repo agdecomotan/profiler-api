69a05cf5de32ec84e8f50e2078a6d8edbcb0f5f3
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2019-02-24 22:39:53 +0800

    Added models

---
+++ models/student.php
@@ -0,0 +1,25 @@
+<?php
+namespace AGD\Profiler;
+class Student
+{
+    public $id;
+    public $studentNumber;
+    public $firstName;
+    public $lastName;    
+    public $yearLevel; 
+    public $program;
+    public $email;
+
+    public function __construct(array $data)
+    {
+        if ($data !== null) {
+            $this->id = (int) $data['id'] ?? 0;
+            $this->studentNumber = $data['studentNumber'] ?? null;
+            $this->firstName = $data['firstName'] ?? null;
+            $this->lastName = $data['lastName'] ?? null;
+            $this->yearLevel = $data['yearLevel'] ?? null;
+            $this->program = $data['program'] ?? null;
+            $this->email = $data['email'] ?? null;
+        }
+    }
+}
