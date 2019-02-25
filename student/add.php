fec0157c0ae5754ae04fcfc320901dfb24d3ee8b
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2019-02-25 16:15:33 +0800

    Add student

---
+++ student/add.php
@@ -0,0 +1,44 @@
+<?php
+namespace AGD\Wis;
+
+require $_SERVER['DOCUMENT_ROOT'] . '/profiler-api/utils/db.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/profiler-api/utils/http.php';
+
+use Exception;
+use PDOException;
+
+Http::HandleOption();
+Http::SetDefaultHeaders('POST');
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    Http::ReturnError(405, array('message' => 'Request method is not allowed.'));
+    return;
+}
+
+$input = json_decode(file_get_contents("php://input"));
+$datecreated = date('Y-m-d H:i:s');
+
+if (is_null($input)) {
+    Http::ReturnError(400, array('message' => 'Object details are empty.'));
+} else {
+    try {
+        $db = new Db('INSERT INTO `students`(studentNumber, firstName, lastName, yearLevel, program, email) 
+                    VALUES(:studentNumber, :firstName, :lastName, :yearLevel, :program, :email)');
+        
+        $db->bindParam(':studentNumber', property_exists($input, 'studentNumber') ? $input->studentNumber : null);
+        $db->bindParam(':firstName', property_exists($input, 'firstName') ? $input->firstName : null);
+        $db->bindParam(':lastName', property_exists($input, 'lastName') ? $input->lastName : null);
+        $db->bindParam(':yearLevel', property_exists($input, 'yearLevel') ? $input->yearLevel : null);    
+        $db->bindParam(':program', property_exists($input, 'program') ? $input->program : null);   
+        $db->bindParam(':email', property_exists($input, 'email') ? $input->email : null);     
+        //$db->bindParam(':datecreated', $datecreated);
+        
+        $db->execute();
+        $id = $db->lastInsertId();
+        $db->commit();
+        Http::ReturnSuccess(array('message' => 'Object created.', 'id' => $input->id));
+    } catch (PDOException $pe) {
+        Db::ReturnDbError($pe);
+    } catch (Exception $e) {
+        Http::ReturnError(500, array('message' => 'Server error: ' . $e->getMessage() . '.'));
+    }
+}
