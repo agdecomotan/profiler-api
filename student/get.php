7474234ea4e465ddc8b0866228961bac8a20943d
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2019-02-25 17:11:12 +0800

    Update student api

--- student/get.php
+++ student/get.php
@@ -3,7 +3,7 @@ namespace AGD\Profiler;
 
 require $_SERVER['DOCUMENT_ROOT'] . '/profiler-api/utils/db.php';
 require $_SERVER['DOCUMENT_ROOT'] . '/profiler-api/utils/http.php';
-require $_SERVER['DOCUMENT_ROOT'] . '/profiler-api/models/student.php'
+require $_SERVER['DOCUMENT_ROOT'] . '/profiler-api/models/student.php';
 
 use Exception;
 use PDOException;
@@ -23,15 +23,15 @@ if (array_key_exists('id', $_GET)) {
 
 try {
     if ($id === 0) {
-        $db = new Db('SELECT * FROM `students`'); 
-        $db->execute();
-        $records = $db->fetchAll();
-        $rowCount = count($records);
-        if ($rowCount === 0) {
-            Http::ReturnError(404, array('message' => 'No records.'));
-        } else {      
-            Http::ReturnSuccess($records);
-        }
+        // $db = new Db('SELECT * FROM `students`'); 
+        // $db->execute();
+        // $records = $db->fetchAll();
+        // $rowCount = count($records);
+        // if ($rowCount === 0) {
+        //     Http::ReturnError(404, array('message' => 'No records.'));
+        // } else {      
+        //     Http::ReturnSuccess($records);
+        // }
 
 
         $db = new Db('SELECT * FROM `students`');
