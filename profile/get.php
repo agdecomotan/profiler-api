0f6a578d8198acf553e903badccfdbc2b6a9542b
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2019-03-14 22:48:02 +0800

    Get status

--- profile/get.php
+++ profile/get.php
@@ -16,13 +16,33 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
 }
 
 $id = 0;
+$status = '';
 
 if (array_key_exists('id', $_GET)) {
     $id = intval($_GET['id']);
 }
 
+if (array_key_exists('status', $_GET)) {
+    $status = $_GET['status'];
+}
+
 try {
-    if ($id === 0) {
+     if ($status !== '') {
+        $db = new Db('SELECT * FROM profiles g JOIN students c ON g.studentId = c.id');
+        $response = array();
+        $db->execute();
+        $records = $db->fetchAll();
+        $rowCount = count($records);
+        if ($rowCount > 0) {
+            foreach ($records as &$record) {
+                $value = new Profile($record);
+                array_push($response, $value);
+            }
+            Http::ReturnSuccess($response);
+        } else {      
+            Http::ReturnError(404, array('message' => 'No records.'));
+        }     
+    } elseif ($id === 0) {
         $db = new Db('SELECT * FROM profiles g JOIN students c ON g.studentId = c.id');
         $response = array();
         $db->execute();
