9d44a8c953a42e330912885cebb1f9a6e6ac3f83
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2019-03-19 21:53:17 +0800

    Update profile

--- profile/add.php
+++ profile/add.php
@@ -21,29 +21,38 @@ if (is_null($input)) {
     Http::ReturnError(400, array('message' => 'Object details are empty.'));
 } else {
     try {
-        $db = new Db('INSERT INTO `profiles`(dateCreated, initialDate, finalDate, initialResult, finalResult, status, studentId, userId, sdExam, dsExam, msExam, sdInterview, dsInterview, msInterview) 
-                    VALUES(:dateCreated, :initialDate, :finalDate, :initialResult, :finalResult, :status, :studentId, :userId, :sdExam, :dsExam, :msExam, :sdInterview, :dsInterview, :msInterview)');
-        
-        $db->bindParam(':dateCreated', $datecreated);
-        $db->bindParam(':initialDate', property_exists($input, 'initialDate') ? $input->initialDate : null);
-        $db->bindParam(':finalDate', property_exists($input, 'finalDate') ? $input->finalDate : null);
-        $db->bindParam(':initialResult', property_exists($input, 'initialResult') ? $input->initialResult : null);    
-        $db->bindParam(':finalResult', property_exists($input, 'finalResult') ? $input->finalResult : null);  
-        $db->bindParam(':status', property_exists($input, 'status') ? $input->status : null);  
-        $db->bindParam(':studentId', property_exists($input, 'studentId') ? $input->studentId : null);   
-        $db->bindParam(':userId', property_exists($input, 'userId') ? $input->userId : null);     
-        $db->bindParam(':sdExam', property_exists($input, 'sdExam') ? $input->sdExam : null);         
-        $db->bindParam(':dsExam', property_exists($input, 'dsExam') ? $input->dsExam : null);         
-        $db->bindParam(':msExam', property_exists($input, 'msExam') ? $input->msExam : null);         
-        $db->bindParam(':sdInterview', property_exists($input, 'sdInterview') ? $input->sdInterview : null); 
-        $db->bindParam(':dsInterview', property_exists($input, 'dsInterview') ? $input->dsInterview : null);            
-        $db->bindParam(':msInterview', property_exists($input, 'msInterview') ? $input->msInterview : null);             
-       
-        
+        $db = new Db('SELECT * FROM profiles g JOIN students c ON g.studentId = c.id WHERE g.studentId = :studentId LIMIT 1');
+        $db->bindParam(':studentId', $studentId);
         $db->execute();
-        $db->commit();
-        $id = $db->lastInsertId();
-        Http::ReturnSuccess(array('message' => 'Object created.', 'id' => $id));
+        $records = $db->fetchAll();
+        $rowCount = count($records);
+        if ($rowCount === 0) { 
+            $db = new Db('INSERT INTO `profiles`(dateCreated, initialDate, finalDate, initialResult, finalResult, status, studentId, userId, sdExam, dsExam, msExam, sdInterview, dsInterview, msInterview) 
+                        VALUES(:dateCreated, :initialDate, :finalDate, :initialResult, :finalResult, :status, :studentId, :userId, :sdExam, :dsExam, :msExam, :sdInterview, :dsInterview, :msInterview)');
+            
+            $db->bindParam(':dateCreated', $datecreated);
+            $db->bindParam(':initialDate', property_exists($input, 'initialDate') ? $input->initialDate : null);
+            $db->bindParam(':finalDate', property_exists($input, 'finalDate') ? $input->finalDate : null);
+            $db->bindParam(':initialResult', property_exists($input, 'initialResult') ? $input->initialResult : null);    
+            $db->bindParam(':finalResult', property_exists($input, 'finalResult') ? $input->finalResult : null);  
+            $db->bindParam(':status', property_exists($input, 'status') ? $input->status : null);  
+            $db->bindParam(':studentId', property_exists($input, 'studentId') ? $input->studentId : null);   
+            $db->bindParam(':userId', property_exists($input, 'userId') ? $input->userId : null);     
+            $db->bindParam(':sdExam', property_exists($input, 'sdExam') ? $input->sdExam : null);         
+            $db->bindParam(':dsExam', property_exists($input, 'dsExam') ? $input->dsExam : null);         
+            $db->bindParam(':msExam', property_exists($input, 'msExam') ? $input->msExam : null);         
+            $db->bindParam(':sdInterview', property_exists($input, 'sdInterview') ? $input->sdInterview : null); 
+            $db->bindParam(':dsInterview', property_exists($input, 'dsInterview') ? $input->dsInterview : null);            
+            $db->bindParam(':msInterview', property_exists($input, 'msInterview') ? $input->msInterview : null);             
+           
+            
+            $db->execute();
+            $db->commit();
+            $id = $db->lastInsertId();
+            Http::ReturnSuccess(array('message' => 'Object created.', 'id' => $id));
+        } else {
+            Http::ReturnSuccess(array('message' => 'Object exists.', 'id' => $studentId));
+        }
     } catch (PDOException $pe) {
         Db::ReturnDbError($pe);
     } catch (Exception $e) {
