d1dc89e21fcecc23335da1ff3df2fe3136e07060
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2019-03-19 22:23:23 +0800

    Update email message

--- profile/email.php
+++ profile/email.php
@@ -42,7 +42,7 @@ try {
   	$mail->setFrom($mail->Username); 
   	$mail->addAddress($value->email); 
   	$mail->Subject = 'Your Account Registration';   	
-  	$message = '<p>Dear '.$value->studentFirstName.',</p><p>Thank you for registering to my website. Have a great day!</p>'; 
+  	$message = '<p>Dear '.$value->studentFirstName.',</p><p>Here is the result of the track profiling: '.$value->finalResult'.</p>'; 
   	$mail->msgHTML($message); 
   	$mail->AltBody = strip_tags($message); 
   	if(!$mail->send())
