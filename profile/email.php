b4e8b34960281b99ccfa20b1c202936af2c71880
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2019-03-19 21:06:26 +0800

    Update email

--- profile/email.php
+++ profile/email.php
@@ -28,7 +28,7 @@ try {
 	    $record = $records[0];
 	    $value = new Profile($record);            
 	}
-	Http::ReturnSuccess($value.email);
+	Http::ReturnSuccess($value);
 
   	$mail = new PHPMailer(true);
   	$mail->isSMTP();  
@@ -40,9 +40,9 @@ try {
   	$mail->Username = "agdecomotan@up.edu.ph"; 
   	$mail->Password = "fnggtebfexgawxyg"; 
   	$mail->setFrom($mail->Username); 
-  	$mail->addAddress("agdecomotan@yahoo.com"); 
+  	$mail->addAddress($value->email); 
   	$mail->Subject = 'Your Account Registration';   	
-  	$message = '<p>Dear ,</p><p>Thank you for registering to my website. Have a great day!</p>'; 
+  	$message = '<p>Dear '.$value->studentFirstName.',</p><p>Thank you for registering to my website. Have a great day!</p>'; 
   	$mail->msgHTML($message); 
   	$mail->AltBody = strip_tags($message); 
   	if(!$mail->send())
