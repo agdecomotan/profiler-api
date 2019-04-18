aa7dafae82f50c35b526f993d449e756dc6f6f1e
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2019-04-19 07:03:15 +0800

    Add setting

---
+++ models/setting.php
@@ -0,0 +1,18 @@
+<?php
+<?php
+namespace AGD\Profiler;
+class Setting
+{
+    public $id;
+    public $name;
+    public $value;    
+
+    public function __construct(array $data)
+    {
+        if ($data !== null) {
+            $this->id = (int) $data['id'] ?? 0;
+            $this->name = $data['name'] ?? null;
+            $this->value = $data['value'] ?? null;
+        }
+    }
+}
