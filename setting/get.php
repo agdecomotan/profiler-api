d990a5ed2e595a80ee698672e2eff21742cf23a9
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2019-04-19 07:02:46 +0800

    Added settings

---
+++ setting/get.php
@@ -0,0 +1,54 @@
+<?php
+namespace AGD\Profiler;
+
+require $_SERVER['DOCUMENT_ROOT'] . '/profiler-api/utils/db.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/profiler-api/utils/http.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/profiler-api/models/setting.php';
+
+use Exception;
+use PDOException;
+
+Http::SetDefaultHeaders('GET');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    Http::ReturnError(405, array('message' => 'Request method is not allowed.'));
+    return;
+}
+
+$id = 0;
+
+if (array_key_exists('id', $_GET)) {
+    $id = intval($_GET['id']);
+}
+
+try {
+    if ($id === 0) {
+        $db = new Db('SELECT * FROM `settings`');
+        $response = array();
+        $db->execute();
+        $records = $db->fetchAll();
+        $rowCount = count($records);
+        if ($rowCount > 0) {
+            foreach ($records as &$record) {
+                $value = new Student($record);
+                array_push($response, $value);
+            }
+        }  
+        Http::ReturnSuccess($response);
+    } else {
+        $db = new Db('SELECT * FROM `settings` WHERE id = :id LIMIT 1');
+        $db->bindParam(':id', $id);
+        $db->execute();
+        $records = $db->fetchAll();
+        $rowCount = count($records);
+        if ($rowCount > 0) {
+            $record = $records[0];
+            $value = new Student($record);            
+        }
+        Http::ReturnSuccess($value);
+    }
+} catch (PDOException $pe) {
+    Db::ReturnDbError($pe);
+} catch (Exception $e) {
+    Http::ReturnError(500, array('message' => 'Server error: ' . $e->getMessage() . '.'));
+}
